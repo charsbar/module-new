913a20c5ea9a24af3766ff40b0ec641ee33ce02a
Kenichi Ishigaki <ishigaki@cpan.org>
2013-09-27 00:20:53 +0900

    s/TEST_POD/RELEASE_TESTING/g

--- lib/Module/New/File/PodTest.pm
+++ lib/Module/New/File/PodTest.pm
@@ -11,7 +11,7 @@ use Test::More;
 
 eval "use Test::Pod 1.18";
 plan skip_all => 'Test::Pod 1.18 required' if $@;
-plan skip_all => 'set TEST_POD to enable this test' unless $ENV{RELEASE_TESTING};
+plan skip_all => 'set RELEASE_TESTING to enable this test' unless $ENV{RELEASE_TESTING};
 all_pod_files_ok();
 EOT
 };
