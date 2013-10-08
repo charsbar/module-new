88a1f308bea0d989a8ba1587b21c09b4a5571af6
Kenichi Ishigaki <ishigaki@cpan.org>
2013-10-08 15:55:30 +0900

    orz, forgot to fix/modify tests

--- t/lib/Module/New/Test/License/Basic.pm
+++ t/lib/Module/New/Test/License/Basic.pm
@@ -10,20 +10,20 @@ sub initialize {
   my $class = shift;
 
   my $license_class = $class->license_class;
-  eval "require $license_class";
+  eval "use $license_class";
 }
 
 sub perl : Test {
   my $class = shift;
 
-  my $license = $class->license_class->render('perl');
+  my $license = $class->license_class->object('perl', {holder => 'me', year => '2013'});
   ok $license, $class->message('found license');
 }
 
 sub not_found : Test {
   my $class = shift;
 
-  eval { $class->license_class->render('not_found') };
+  eval { $class->license_class->object('not_found', {holder => 'me', year => '2013'}) };
   ok $@, $class->message('not found license');
 }
 
